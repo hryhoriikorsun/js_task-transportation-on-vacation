8258ea1b86a1ec0fd8a38a22aa7ceba578049cd4
Gregory Korsun <lvlup9119@gmail.com>
2025-04-22 12:31:03 +0300

    Add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let counter = 0;
+  const PRICE_ONE_DAY = 40;
+  const DISCAUNT_3_DAYS = 20;
+  const DISCAUNT_7_DAYS = 50;
+
+  for (let i = 0; i < days; i++) {
+    counter += PRICE_ONE_DAY;
+  }
+
+  if (days >= 3) {
+    counter = counter - DISCAUNT_3_DAYS;
+  }
+
+  if (days >= 7) {
+    counter = counter - (DISCAUNT_7_DAYS - DISCAUNT_3_DAYS);
+  }
+
+  return counter;
 }
 
 module.exports = calculateRentalCost;
